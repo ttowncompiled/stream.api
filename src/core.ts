65972555a6da20082fd58be9f3991e6b03961af2
Ian Riley <ian.riley.915@gmail.com>
2015-08-09 02:34:14 -0700

    feat(disposable): Adds dispose pattern.

--- src/core.ts
+++ src/core.ts
@@ -1,10 +1,14 @@
+export interface Disposable {
+  dispose: () => void;
+}
+
 export interface Observer<T> {
   complete: () => void;
   error: (err: any) => void;
   next: (item?: T) => void;
 }
 
-export interface Observable<T> {
+export interface Observable<T> extends Disposable {
   subscribe: (observer: Observer<T>) => void;
 }
 
