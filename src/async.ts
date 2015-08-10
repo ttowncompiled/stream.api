f01f2680d9f3a9440427f85efac439eb6004816a
Ian Riley <ian.riley.915@gmail.com>
2015-08-10 13:48:58 -0700

    feat(async): Defines subject interface for the asynchronous implementation of the observer api.

--- src/async.ts
+++ src/async.ts
@@ -1,3 +1,4 @@
 export { Scheduler } from './async/async_scheduler';
 export { Observable } from './async/async_observable';
+export { Subject } from './async/async_subject';
 
--- test/async_spec.ts
+++ test/async_spec.ts
@@ -1,9 +1,10 @@
 /// <reference path="../typings/tsd.d.ts" />
 import { expect } from 'chai';
-import { OnComplete, OnNext } from '../src/types';
+import { OnComplete, OnError, OnNext } from '../src/types';
 import { Generator, Observer } from '../src/core';
 import { Scheduler } from '../src/async/async_scheduler';
 import { ColdObservable, DeferredObservable, HotObservable, Observable } from '../src/async/async_observable';
+import { Subject } from '../src/async/async_subject';
 
 describe('Scheduler', () => {
 
@@ -568,3 +569,111 @@ describe('Hot Observable', () => {
 
 });
 
+describe('Subject', () => {
+  
+  it('should emit notifications to all of its subscribers', done => {
+    let sequence: number[] = [ 1, 2, 3 ];
+    let pending: number[] = [];
+    let subject: Subject<number> = new Subject<number>();
+    sequence.forEach(object => subject.next(object));
+    let subscriber: OnNext<number> = object => {
+      if (sequence.indexOf(object) > -1) {
+        sequence.splice(sequence.indexOf(object), 1);
+        pending.push(object);
+      } else {
+        expect(pending.indexOf(object)).to.be.above(-1);
+        pending.splice(pending.indexOf(object), 1);
+      }
+      if (sequence.length === 0 && pending.length === 0) done(); 
+    };
+    subject.subscribeOnNext(subscriber);
+    subject.subscribeOnNext(subscriber);
+  });
+
+  it('should emit errors to all of its subscribers', done => {
+    let sequence: number[] = [ 1, 2, 3 ];
+    let pending: number[] = [];
+    let subject: Subject<number> = new Subject<number>();
+    sequence.forEach(object => subject.error(object));
+    let subscriber: OnError = (object: number) => {
+      if (sequence.indexOf(object) > -1) {
+        sequence.splice(sequence.indexOf(object), 1);
+        pending.push(object);
+      } else {
+        expect(pending.indexOf(object)).to.be.above(-1);
+        pending.splice(pending.indexOf(object), 1);
+      }
+      if (sequence.length === 0 && pending.length === 0) done(); 
+    };
+    subject.subscribeOnError(subscriber);
+    subject.subscribeOnError(subscriber);
+
+  });
+
+  it('should complete all of its subscribers upon completion', done => {
+    let completed: boolean = false;
+    let subject: Subject<void> = new Subject<void>();
+    subject.complete();
+    let subscriber: OnComplete<void> = () => {
+      if (completed) done();
+      completed = true;
+    };
+    subject.subscribeOnComplete(subscriber);
+    subject.subscribeOnComplete(subscriber);
+  });
+
+  it('should be disposed when all of its subscriptions complete', done => {
+    let cb: Generator<void> = observer => observer.complete();
+    let observable: ColdObservable<void> = new ColdObservable<void>(cb);
+    let subject: Subject<void> = new Subject<void>();
+    subject.subscribeTo(observable);
+    subject.subscribeOnComplete(() => {
+      expect(subject._subscribers).to.be.empty;
+      expect(subject._subscriptions).to.be.empty;
+      expect(subject.isDisposed).to.be.true;
+      done();
+    });
+  });
+
+  it('should act as an observer', done => {
+    let nextFlag: boolean = true;
+    let errorFlag: boolean = true;
+    let cb: Generator<void> = observer => {
+      observer.next();
+      observer.error(null);
+      observer.complete();
+    };
+    let observable: ColdObservable<void> = new ColdObservable<void>(cb);
+    let subject: Subject<void> = new Subject<void>();
+    subject.subscribeTo(observable);
+    let subscriber: Observer<void> = {
+      complete: () => { if (nextFlag && errorFlag) done(); },
+      error: err => errorFlag = true,
+      next: () => nextFlag = true
+    };
+    subject.subscribe(subscriber);
+  });
+
+  it('should lazily subscribe', done => {
+    let cb: Generator<void> = observer => done(new Error('cold generator was called'));
+    let observable: ColdObservable<void> = new ColdObservable<void>(cb);
+    new Subject<void>().subscribeTo(observable);
+    done();
+  });
+
+  it('should handle mutiple subscriptions', done => {
+    let zero: number = 0;
+    let flag: boolean = false;
+    let cb: Generator<number> = observer => observer.next(zero);
+    let observable: ColdObservable<number> = new ColdObservable<number>(cb);
+    let subject: Subject<number> = new Subject<number>();
+    subject.subscribeTo(observable);
+    subject.subscribeTo(observable);
+    subject.subscribeOnNext(object => {
+      expect(object).to.equal(zero);
+      if (flag) done();
+      flag = true;
+    });
+  });
+ 
+});
